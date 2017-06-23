830c07f59bc8e37fe3b5a604028c159b96e15f22
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-06-23 16:46:22 +0100

    Create app.py
    Establish connection to SQL database

---
+++ Flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("test.html")
+
+    
+def connect_to_database():
+    db_str = "mysql+mysqldb://{}:{}@{}:{}/{}"
+    engine = create_engine(db_str.format(config.USER,
+                                        config.PASSWORD,
+                                        config.URI,
+                                        config.PORT,
+                                        config.DB),
+                           echo=True)
+    return engine
+
+def get_db():
+    engine = getattr(g, 'engine', None)
+    if engine is None:
+        engine = g.engine = connect_to_database()
+    return engine
