bafd5ba3a16819cefa6ff749c5c129bedead947a
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-06-30 18:41:03 +0100

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_tmeplate
+from flask import Flask, render_tmeplate, g, jsonify
 from jinja2 import Template
 from sqlalchemy import create_engine
 import MySQLdb
@@ -31,13 +31,13 @@ def close_connection(exception):
 def index():
     return render_template('index.html')
 
-@app.route("/route/<int:busnum>")
-def get_route(busnum):
+@app.route("/route")
+def get_route():
     engine = get_db()
     data = []
-    rows = engine.execute("""SELECT applicantid, firstname FROM applicant WHERE applicantid = {};""".format(busnum))
-        for row in rows:
-            data.append(dict(row))
+    rows = engine.execute("""SELECT DISTINCT LineID FROM dublinbus;""".format(config.DB))
+    for row in rows:
+        data.append(dict(row))
     return jsonify(available=data)
 
 @app.route("/origin/<int:stopnum")
@@ -45,7 +45,7 @@ def route_ori(stopnum):
     engine = get_db()
     sql = """SELECT StopID FROM dublinbus WHERE stopnum = {};""".format(stopnum)
     rows = engine.execute(sql).fetchall()
-    stopnum = jsonify(stations=[dict9row.items()0 for row in rows])
+    stopnum = jsonify(stations=[dict(row.items()) for row in rows])
     return stations
 
 @app.route("/destination/<int:stopnum")
@@ -57,7 +57,7 @@ def route_dest(stopnum):
     return stations
 
 @app.route('/weather/<string:condition>')
-def get_occupied(condition):
+def get_weather(condition):
     engine = get_db()
     data = []
     rows = engine.execute("SELECT precipitation, temperature FROM dublinbus WHERE weather = {};".format(condition))
