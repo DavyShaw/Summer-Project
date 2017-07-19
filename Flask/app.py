c10e54c5125d93b1e3f3573bd0af4b7ab7a6dd49
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-07-19 11:36:58 +0100

    Update app.py
    Access to JSON file implemented for dropdown menus

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, g, jsonify
-from jinja2 import Template
+import config
 from sqlalchemy import create_engine
 import MySQLdb
 import json
-
 app = Flask(__name__)
 
+    
 def connect_to_database():
     db_str = "mysql+mysqldb://{}:{}@{}:{}/{}"
     engine = create_engine(db_str.format(config.USER,
@@ -22,52 +22,34 @@ def get_db():
         engine = g.engine = connect_to_database()
     return engine
 
+@app.route("/")
+def main():
+    with open('static/dublinbus_routes.json') as data_file:    
+        json_file_routes = json.load(data_file)
+        
+    with open('static/all_stops_and_routesDB.json') as data_file:
+        json_file_stops = json.load(data_file)
+    
+    return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops)
+
+#@app.route("/routes")
+#def main():
+#    with open('static/dublinbus_routes.json') as data_file:    
+#        json_file_routes = json.load(data_file)
+#        
+#    with open('static/all_stops_and_routesDB.json') as data_file:
+#        json_file_stops = json.load(data_file)
+#    
+#    return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops)
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
         
-@app.route("/")
-def main():
-    with open('templates/convertcsv.json') as data_file:    
-        json_file = json.load(data_file)
-    return render_template("index.html", json_file = json_file)
 
-@app.route("/route")
-def get_route():
-    engine = get_db()
-    data = []
-    rows = engine.execute("""SELECT DISTINCT LineID FROM dublinbus;""".format(config.DB))
-    for row in rows:
-        data.append(dict(row))
-    return jsonify(available=data)
 
-@app.route("/origin/<int:stopnum>")
-def route_ori(stopnum):
-    engine = get_db()
-    sql = """SELECT StopID FROM dublinbus WHERE stopnum = {};""".format(stopnum)
-    rows = engine.execute(sql).fetchall()
-    stopnum = jsonify(stations=[dict(row.items()) for row in rows])
-    return stations
-
-@app.route("/destination/<int:stopnum>")
-def route_dest(stopnum):
-    engine = get_db()
-    sql = """SELECT StopID FROM dublinbus WHERE stopnum = {};""".format(stopnum)
-    rows = engine.execute(sql).fetchall()
-    stopnum = jsonify(stations=[dict(row.items()) for row in rows])
-    return stations
-
-@app.route('/weather/<string:condition>')
-def get_weather(condition):
-    engine = get_db()
-    data = []
-    rows = engine.execute("SELECT precipitation, temperature FROM dublinbus WHERE weather = {};".format(condition))
-    for row in rows:
-        data.append(dict(row))
-    return jsonify(available=data)
-    
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
