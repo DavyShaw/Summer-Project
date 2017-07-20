f48499288a5f67291b761654b382f5c4b5374cac
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-07-20 19:20:08 +0100

    Beginning functionality implemented
    Updated app.py, new function used to gather user inputs - returning variables to Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, jsonify
+from flask import Flask, render_template, g, jsonify, request
 import config
 from sqlalchemy import create_engine
 import MySQLdb
@@ -32,15 +32,16 @@ def main():
     
     return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops)
 
-#@app.route("/routes")
-#def main():
-#    with open('static/dublinbus_routes.json') as data_file:    
-#        json_file_routes = json.load(data_file)
-#        
-#    with open('static/all_stops_and_routesDB.json') as data_file:
-#        json_file_stops = json.load(data_file)
-#    
-#    return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops)
+@app.route("/routes", methods=['GET','POST'])
+def routes():
+    chosenroute = request.form.get('chosenroute')
+    chosenorigin = request.form.get('chosenorigin')
+    chosendestination = request.form.get('chosendestination')
+    chosenday = request.form.get('chosenday')
+    chosentime = request.form.get('chosentime')
+    chosenweather = request.form.get('chosenweather')
+
+    return(str(chosenroute +" "+ chosenorigin+" "+ chosendestination+" "+ chosenday+" "+ chosentime+" "+ chosenweather))
 
 
 @app.teardown_appcontext
