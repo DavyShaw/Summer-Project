3740815bfd73b9a4a3d766580ba177185e5d1101
claudia-kinsella <claudia.kinsella@ucdconnect.ie>
2017-07-21 13:30:28 +0100

    Improved functionality
    Second display page implemented to show user inputs, new json file displaying more accurate stopIDs and routes

--- Flask/app.py
+++ Flask/app.py
@@ -27,10 +27,13 @@ def main():
     with open('static/dublinbus_routes.json') as data_file:    
         json_file_routes = json.load(data_file)
         
+    with open('static/routes.json') as data_file:
+        json_routes = json.load(data_file)
+        
     with open('static/all_stops_and_routesDB.json') as data_file:
         json_file_stops = json.load(data_file)
     
-    return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops)
+    return render_template("index.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops, json_routes = json_routes)
 
 @app.route("/routes", methods=['GET','POST'])
 def routes():
@@ -40,9 +43,31 @@ def routes():
     chosenday = request.form.get('chosenday')
     chosentime = request.form.get('chosentime')
     chosenweather = request.form.get('chosenweather')
+    
+    with open('static/dublinbus_routes.json') as data_file:    
+        json_file_routes = json.load(data_file)
+        
+    with open('static/routes.json') as data_file:
+        json_routes = json.load(data_file)
+        
+    with open('static/all_stops_and_routesDB.json') as data_file:
+        json_file_stops = json.load(data_file)
+    
+    return render_template("display.html", json_file_routes = json_file_routes, json_file_stops = json_file_stops, json_routes = json_routes, chosenroute = chosenroute)
 
-    return(str(chosenroute +" "+ chosenorigin+" "+ chosendestination+" "+ chosenday+" "+ chosentime+" "+ chosenweather))
+#    return(str(chosenroute +" "+ chosenorigin+" "+ chosendestination+" "+ chosenday+" "+ chosentime+" "+ chosenweather)) 
+#    return render_template("index.html")
 
+#@app.route("/routes", methods=['GET','POST'])
+#def routes():
+#    chosenroute = request.form.get('chosenroute')
+#    chosenorigin = request.form.get('chosenorigin')
+#    chosendestination = request.form.get('chosendestination')
+#    chosenday = request.form.get('chosenday')
+#    chosentime = request.form.get('chosentime')
+#    chosenweather = request.form.get('chosenweather')
+#
+#    return render_template('index.html')
 
 @app.teardown_appcontext
 def close_connection(exception):
